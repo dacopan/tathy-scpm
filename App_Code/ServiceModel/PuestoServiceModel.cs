08344eac5249505db7b7f1bdc57fa9a3cfd479d5
Darwin Correa <dacopanbsc@gmail.com>
2015-09-01 18:25:20 -0500

    add date range validate on history fijo

--- App_Code/ServiceModel/PuestoServiceModel.cs
+++ App_Code/ServiceModel/PuestoServiceModel.cs
@@ -17,6 +17,7 @@ public class PuestoServiceModel
         // TODO: Agregar aquí la lógica del constructor
         //
     }
+
     public List<SCPM_UNIDAD> getAllUnidades()
     {
         return db.SCPM_UNIDAD.ToList();
@@ -184,11 +185,36 @@ public class PuestoServiceModel
         }
     }
 
+    public SCPM_CARGOS getCargoByID(int cargo_id)
+    {
+        return (from a in db.SCPM_CARGOS where a.CAR_ID == cargo_id select a).FirstOrDefault();
+    }
+
+    private SCPM_RELACIONES_LABORALES getRalacionLabByID(int rel_lab_id)
+    {
+        return (from a in db.SCPM_RELACIONES_LABORALES where a.REL_LAB_ID == rel_lab_id select a).FirstOrDefault();
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="p"></param>
+    /// <returns></returns>
+    public List<SCPM_PERSONALES> getPersonasByID(int p)
+    {
+        return (from a in db.SCPM_PERSONALES where a.PER_ID == p select a).ToList();
+    }
+
+    public List<SCPM_PERSONALES> getAllPersonas()
+    {
+        return db.SCPM_PERSONALES.ToList();
+    }
+
     public bool addCargoHistorial(SCPM_PUESTO_HIST historial, int per_id, int cargo_id, int rel_lab_id)
     {
         //compruebo si persona ya tiene un cargo activo
-        var ccp = (from h in db.SCPM_PUESTO_HIST.Include("SCPM_PERSONALES") where h.SCPM_PERSONALES.PER_ID == per_id select h).ToList();
-        SCPM_PUESTO_HIST currentCargo = ccp.Count > 0 ? ccp.OrderByDescending(c => c.PST_HIS_FEC_INI).FirstOrDefault() : ccp.FirstOrDefault();
+        var historial_cargos_persona = (from h in db.SCPM_PUESTO_HIST.Include("SCPM_PERSONALES") where h.SCPM_PERSONALES.PER_ID == per_id select h).ToList();
+        SCPM_PUESTO_HIST currentCargo = historial_cargos_persona.Count > 0 ? historial_cargos_persona.OrderByDescending(c => c.PST_HIS_FEC_INI).FirstOrDefault() : historial_cargos_persona.FirstOrDefault();
 
         if (currentCargo != null)
         {
@@ -198,18 +224,17 @@ public class PuestoServiceModel
 
         if (currentCargo != null && currentCargo.SCPM_CARGOS.CAR_ID != cargo_id && (currentCargo.PST_HIS_FEC_FIN == null || DateTime.Now.CompareTo(currentCargo.PST_HIS_FEC_FIN) <= 0))
         {
-            HelperUtil.showNotifi("persona ya tiene un cargo activo, finalicelo para podr asignarlo a otro cargo");
+            HelperUtil.showNotifi("persona ya tiene un cargo activo, finalicelo para poder asignarlo a otro cargo");
             return false;
         }
 
         //
-        var cc = (from h in db.SCPM_PUESTO_HIST.Include("SCPM_CARGOS") where h.SCPM_CARGOS.CAR_ID == cargo_id select h).ToList();
+        var historial_cargos_cargo_id = (from h in db.SCPM_PUESTO_HIST.Include("SCPM_CARGOS") where h.SCPM_CARGOS.CAR_ID == cargo_id select h).ToList();
         var nowTime = DateTime.Now;
 
-        SCPM_PUESTO_HIST lastCargo = cc.Count > 0 ? cc.OrderByDescending(c => c.PST_HIS_FEC_INI).FirstOrDefault() : cc.FirstOrDefault();
+        SCPM_PUESTO_HIST lastCargo = historial_cargos_cargo_id.Count > 0 ? historial_cargos_cargo_id.OrderByDescending(c => c.PST_HIS_FEC_INI).FirstOrDefault() : historial_cargos_cargo_id.FirstOrDefault();
 
         if (lastCargo != null && (lastCargo.PST_HIS_FEC_FIN == null || DateTime.Now.CompareTo(lastCargo.PST_HIS_FEC_FIN) <= 0))
-        //if (lastCargo != null && lastCargo.PST_HIS_FEC_FIN == null)
         {//el cargo esta actualmente ocupado
 
             lastCargo.SCPM_PERSONALESReference.Load();
@@ -226,31 +251,76 @@ public class PuestoServiceModel
                 {// el cargo esta ocupado por la misma persona y relacion laboral es la misma solo se actualizara sus campos
 
                     //fecha fin no puede ser menor a fecha inicio
-                    if (historial.PST_HIS_FEC_FIN != null && lastCargo.PST_HIS_FEC_INI.Value.CompareTo(historial.PST_HIS_FEC_FIN) > 0)
+                    if (historial.PST_HIS_FEC_FIN != null && historial.PST_HIS_FEC_INI.Value.CompareTo(historial.PST_HIS_FEC_FIN) > 0)
                     {
                         HelperUtil.showNotifi("fecha inicio no puede ser mayor que final");
                         return false;
                     }
+                    //checa si rango de fecha no overlap con alguno del historial del cargo o la persona
+                    if (!isRangoFechaCargoValid(historial_cargos_persona, historial_cargos_cargo_id, historial.PST_HIS_FEC_INI.Value, historial.PST_HIS_FEC_FIN, lastCargo))
+                    {
+                        return false;
+                    }
                     lastCargo.PST_HIS_FEC_FIN = historial.PST_HIS_FEC_FIN;
+                    lastCargo.PST_HIS_FEC_INI = historial.PST_HIS_FEC_INI;
                     db.SaveChanges();
                     HelperUtil.showNotifi("Solo fecha final actualizada");
                     return true;
                 }
                 else
-                { // el cargo esta ocupado por la misma persona y relacion laboral no es la misma, se finaliza este historial y añade otro con la nueva relacion laboral
-                    lastCargo.PST_HIS_FEC_FIN = nowTime;
-                    db.SaveChanges();
-                    HelperUtil.showNotifi("Relacion laboral anterior finalizada.");
-                    //add new relacion
+                { // el cargo esta ocupado por la misma persona y relacion laboral no es la misma
+
+                    if (historial.PST_HIS_FEC_FIN == null || historial.PST_HIS_FEC_FIN.Value.CompareTo(nowTime) > 0)
+                    {//quiere cambiar de relacion laboral al cargo que actualmente tiene, se finaliza este historial y añade otro con la nueva relacion laboral
+                        //entonces fecha inicio del nuevo historial debe ser mayor a fecha inicio del cargo actual
+                        if (historial.PST_HIS_FEC_INI.Value.CompareTo(lastCargo.PST_HIS_FEC_INI.Value) < 0)
+                        {
+                            HelperUtil.showNotifi("Fecha inico de la nueva relacion laboral debe ser mayor a fecha inicio de la actual relacion laboral.");
+                            return false;
+                        }
+                        //fecha fin no puede ser menor a fecha inicio
+                        if (historial.PST_HIS_FEC_FIN != null && nowTime.CompareTo(historial.PST_HIS_FEC_FIN) > 0)
+                        {
+                            HelperUtil.showNotifi("fecha inicio no puede ser mayor que final");
+                            return false;
+                        }
+                        lastCargo.PST_HIS_FEC_FIN = nowTime;
+                        historial.PST_HIS_FEC_INI = nowTime;
+                        db.SaveChanges();
+                        HelperUtil.showNotifi("Relacion laboral anterior finalizada.");
+                    }
+                    else
+                    {//el cargo esta actualmente ocupado se desea añadir un historial antiguo
+                        //entonces fecha fin debe ser menor a fecha inicio del cargo actual
+                        if (historial.PST_HIS_FEC_FIN.Value.CompareTo(lastCargo.PST_HIS_FEC_INI) > 0)
+                        {
+                            HelperUtil.showNotifi("Intenta añadir un historial antiguo. fecha final debe ser menor a fecha inicial del cargo actual");
+                            return false;
+                        }
+                        //fecha fin no puede ser menor a fecha inicio
+                        if (historial.PST_HIS_FEC_INI.Value.CompareTo(historial.PST_HIS_FEC_FIN) > 0)
+                        {
+                            HelperUtil.showNotifi("fecha inicio no puede ser mayor que final");
+                            return false;
+                        }
+                        //checa si rango de fecha no overlap con alguno del historial del cargo o la persona
+                        if (!isRangoFechaCargoValid(historial_cargos_persona, historial_cargos_cargo_id, historial.PST_HIS_FEC_INI.Value, historial.PST_HIS_FEC_FIN, null))
+                        {
+                            return false;
+                        }
+
+                    }
+
+                    //add new relacion                       
                     historial.SCPM_PERSONALES = getPersonasByID(per_id).ToList().First();
                     historial.SCPM_RELACIONES_LABORALES = getRalacionLabByID(rel_lab_id);
                     historial.SCPM_CARGOS = getCargoByID(cargo_id);
-                    historial.PST_HIS_FEC_INI = nowTime;
                     db.AddToSCPM_PUESTO_HIST(historial);
                     //save
                     db.SaveChanges();
                     HelperUtil.showNotifi("Nueva Relacion laboral añadida.");
                     return true;
+
                 }
         }
         else
@@ -262,11 +332,11 @@ public class PuestoServiceModel
                 HelperUtil.showNotifi("fecha inicio no puede ser mayor que final");
                 return false;
             }
-            //fecha inicio no puede ser anterior a un historial fecha inicio
-            /*if (lastCargo != null && ) { 
-            HelperUtil.showNotifi("fecha inicio no puede ser menor que fecha anterior historial");
+            //checa si rango de fecha no overlap con alguno del historial del cargo o la persona
+            if (!isRangoFechaCargoValid(historial_cargos_persona, historial_cargos_cargo_id, historial.PST_HIS_FEC_INI.Value, historial.PST_HIS_FEC_FIN, null))
+            {
                 return false;
-            }*/
+            }
             //add new relacion
             historial.SCPM_PERSONALES = getPersonasByID(per_id).ToList().First();
             historial.SCPM_RELACIONES_LABORALES = getRalacionLabByID(rel_lab_id);
@@ -279,34 +349,82 @@ public class PuestoServiceModel
         }
     }
 
-    public SCPM_CARGOS getCargoByID(int cargo_id)
+    private bool isRangoFechaCargoValid(List<SCPM_PUESTO_HIST> historial_cargos_persona, List<SCPM_PUESTO_HIST> historial_cargos_cargo_id, DateTime startB, DateTime? endB, SCPM_PUESTO_HIST lastCargo)
     {
-        return (from a in db.SCPM_CARGOS where a.CAR_ID == cargo_id select a).FirstOrDefault();
+        foreach (var item in historial_cargos_persona)
+        {
+            if (lastCargo == null || item.PST_HIS_ID != lastCargo.PST_HIS_ID)
+            {
+                var startA = item.PST_HIS_FEC_INI.Value;
+                var endA = item.PST_HIS_FEC_FIN;
+                if (endA == null && endB == null)
+                {
+                    if (startA > startB)
+                    {
+                        endA = startA;
+                        endB = startA;
+                    }
+                    else
+                    {
+                        endA = startB;
+                        endB = startB;
+                    }
+
+                }
+                if ((startA <= endB) && (endA >= startB))
+                {
+                    HelperUtil.showNotifi("persona ya posee un cargo en esas fechas");
+                    return false;
+                }
+            }
+        }
+
+        foreach (var item in historial_cargos_cargo_id)
+        {
+            if (lastCargo == null || item.PST_HIS_ID != lastCargo.PST_HIS_ID)
+            {
+                var startA = item.PST_HIS_FEC_INI.Value;
+                var endA = item.PST_HIS_FEC_FIN;
+                if (endA == null && endB == null)
+                {
+                    if (startA > startB)
+                    {
+                        endA = startA;
+                        endB = startA;
+                    }
+                    else
+                    {
+                        endA = startB;
+                        endB = startB;
+                    }
+
+                }
+                if ((startA <= endB) && (endA >= startB))
+                {
+                    HelperUtil.showNotifi("cargo ya posee un funcionario en esas fechas");
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
-    private SCPM_RELACIONES_LABORALES getRalacionLabByID(int rel_lab_id)
-    {
-        return (from a in db.SCPM_RELACIONES_LABORALES where a.REL_LAB_ID == rel_lab_id select a).FirstOrDefault();
-    }
-
-    //from personas service model
-    public List<SCPM_PERSONALES> getPersonasByID(int p)
-    {
-        return (from a in db.SCPM_PERSONALES where a.PER_ID == p select a).ToList();
-    }
-
-    public List<SCPM_PERSONALES> getAllPersonas()
-    {
-        return db.SCPM_PERSONALES.ToList();
-    }
 
 
-
-    //current_puesto_id:        ID: id del cargo a subrogar
-    //current_subroga_id:       id: personaID a la q esta subrogado o encargado     dcm: no esta subrogado
-    //current_tipo_id:          1: si el puesto actual esta subrogado               2: puesto actual encargado          dcm: puesto no sugbrogado           
-    //current_persona_id:       ID: personaID si el puesto esta ocupado             dcm: puesto no ocupado
-    //newPersonaID:             ID: personaID a la cual se subrogará
+    /// <summary>
+    /// añade un registro de subrogacion/encargo al historial
+    /// Falla si el puesto ya esta actualmente sugbrogado/encargado
+    /// </summary>
+    /// <param name="historial"></param>
+    /// <param name="cargo_id">ID: id del cargo a subrogar</param>
+    /// <param name="current_subroga_id">id: personaID a la q esta subrogado o encargado     dcm: no esta subrogado</param>
+    /// <param name="current_tipo_id">1: si el puesto actual esta subrogado               2: puesto actual encargado          dcm: puesto no sugbrogado           </param>
+    /// <param name="current_persona_id">ID: personaID si el puesto esta ocupado             dcm: puesto no ocupado</param>
+    /// <param name="newPersonaID">ID: personaID a la cual se subrogará</param>
+    /// <param name="sub_hist_id"></param>
+    /// <returns>
+    /// true si fue añadido, false si no fue añadido.
+    /// </returns>
     public bool addSubrogaHistorial(SCPM_SUBROGA_HIST historial, int cargo_id, string current_subroga_id, string current_tipo_id, string current_persona_id, int newPersonaID, String sub_hist_id)
     {
         var nowTime = DateTime.Now;
